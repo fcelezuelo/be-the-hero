545fb7ed258e91e6030a8508cf2f83abc9429e85
Fellipe Celezuelo <fellipecelezuelo@gmail.com>
2020-03-28 17:42:16 -0300

    Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './global.css'
+
+import Routes from './routes'
+
+export default function App() {
+  return (
+    <div>
+      <Routes/>
+    </div>
+  );
+}
+
+// export default App;
